f4380a6ea11283fd6bd6bcb88f39e45e8aa7d84d
Grace Lemming <lemming.9@osu.edu>
2023-11-05 20:37:17 -0500

    connected homepage fragment to other fragments

--- app/src/main/java/com/example/stylespo/view/HomepageFragment.java
+++ app/src/main/java/com/example/stylespo/view/HomepageFragment.java
@@ -2,13 +2,36 @@ package com.example.stylespo.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.navigation.fragment.NavHostFragment;
+
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
 
 import com.example.stylespo.R;
+import com.example.stylespo.model.User;
+import com.example.stylespo.viewmodel.MainViewModel;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,7 +40,10 @@ import com.example.stylespo.R;
  */
 public class HomepageFragment extends Fragment {
 
-
+    private MainViewModel mMainViewModel;
+    private FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+    BottomNavigationView bottomNavigationView;
 
     public HomepageFragment() {
         // Required empty public constructor
@@ -30,11 +56,52 @@ public class HomepageFragment extends Fragment {
         super.onCreate(savedInstanceState);
 
     }
+    public void onStop(){
+        super.onStop();
+    }
+
+
+    public void onPause(){
+        super.onPause();
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_homepage, container, false);
+        View v = inflater.inflate(R.layout.fragment_homepage, container, false);
+        bottomNavigationView = v.findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                Fragment fragment = null;
+                int id = item.getItemId();
+                if (id == R.id.AddFragment) {
+                    fragment = new AddFragment();
+                } else if (id == R.id.DiscoverFragment) {
+                    fragment = new DiscoverFragment();
+                } else if (id == R.id.ProfileFragment) {
+                    fragment = new ProfileFragment();
+                } else if (id == R.id.HomepageFragment) {
+                    fragment = new HomepageFragment();
+                }
+             getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.homepage_frag_container, fragment).commit();
+
+
+
+                return true;
+            }
+        });
+        return v;
     }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
 }
+
+
