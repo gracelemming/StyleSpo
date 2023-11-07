fe327e99ce87959fbd57acadbe2bacb15163581b
ankit013002 <patel.3854@osu.edu>
2023-11-07 18:09:00 -0500

    Added drop down menu along with logout feature to profile page.

--- app/src/main/java/com/example/stylespo/view/ProfileFragment.java
+++ app/src/main/java/com/example/stylespo/view/ProfileFragment.java
@@ -1,14 +1,27 @@
 package com.example.stylespo.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import com.example.stylespo.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,7 +73,47 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button showPopupButton = rootView.findViewById(R.id.drop_down_button);
+
+        showPopupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(getActivity(), v);
+                popupMenu.getMenuInflater().inflate(R.menu.drop_down, popupMenu.getMenu());
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        // Handle menu item click here
+                        int itemId = item.getItemId();
+                        if (itemId == R.id.settings_button) {
+                            Toast.makeText(getActivity(),"Settings Selected", Toast.LENGTH_SHORT).show();
+                            return true;
+                        } else if (itemId == R.id.logout_button) {
+                            Toast.makeText(getActivity(),"Logout Selected", Toast.LENGTH_SHORT).show();
+                            // Handle Logout option click
+                            FirebaseAuth.getInstance().signOut();
+                            // Redirect to the login screen or perform any additional cleanup
+                            // For example, navigate to the login screen
+                            // Replace MainActivity.class with your login activity class
+                            Intent intent = new Intent(getActivity(), MainActivity.class);
+                            startActivity(intent);
+                            // Finish the current activity
+                            getActivity().finish();
+                            return true;
+                        } else {
+                            // Add more cases for other menu items as needed
+                            return false;
+                        }
+                    }
+                });
+
+                popupMenu.show();
+            }
+        });
+
+        return rootView;
     }
 }
