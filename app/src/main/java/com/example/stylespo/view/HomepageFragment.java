58b8c3b55ab5adb6bd89ef569ecbacc0014497f4
ankit013002 <patel.3854@osu.edu>
2023-11-09 13:58:55 -0500

    Edited homepage.

--- app/src/main/java/com/example/stylespo/view/HomepageFragment.java
+++ app/src/main/java/com/example/stylespo/view/HomepageFragment.java
@@ -1,51 +1,157 @@
+//package com.example.stylespo.view;
+//
+//import android.os.Bundle;
+//
+//import androidx.annotation.NonNull;
+//import androidx.annotation.Nullable;
+//import androidx.fragment.app.Fragment;
+//
+//
+//import android.view.LayoutInflater;
+//import android.view.MenuItem;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//
+//import com.example.stylespo.R;
+//import com.example.stylespo.viewmodel.MainViewModel;
+//import com.google.android.material.navigation.NavigationBarView;
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.firestore.FirebaseFirestore;
+//
+//
+//import com.google.android.material.bottomnavigation.BottomNavigationView;
+//
+//
+///**
+// * A simple {@link Fragment} subclass.
+// * Use the {@link HomepageFragment#newInstance} factory method to
+// * create an instance of this fragment.
+// */
+//public class HomepageFragment extends Fragment {
+//
+//    private MainViewModel mMainViewModel;
+//    private FirebaseFirestore db;
+//    private FirebaseAuth mAuth;
+//    BottomNavigationView bottomNavigationView;
+//
+//    public HomepageFragment() {
+//        // Required empty public constructor
+//        //
+//    }
+//
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//    }
+//    public void onStop(){
+//        super.onStop();
+//    }
+//
+//
+//    public void onPause(){
+//        super.onPause();
+//    }
+//
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        View v = inflater.inflate(R.layout.fragment_homepage, container, false);
+//        bottomNavigationView = v.findViewById(R.id.bottomNavigationView);
+//
+//        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+//                Fragment fragment = null;
+//                int id = item.getItemId();
+//                if (id == R.id.AddFragment) {
+//                    fragment = new CameraFragment();
+//                } else if (id == R.id.DiscoverFragment) {
+//                    fragment = new DiscoverFragment();
+//                } else if (id == R.id.ProfileFragment) {
+//                    fragment = new ProfileFragment();
+//                } else if (id == R.id.HomepageFragment) {
+//                    fragment = new HomepageFragment();
+//                }
+//             getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.homepage_frag_container, fragment).commit();
+//
+//                return true;
+//            }
+//        });
+//        return v;
+//    }
+//    @Override
+//    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//
+//    }
+//
+//}
+//
+//
+
 package com.example.stylespo.view;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.example.stylespo.R;
-import com.example.stylespo.viewmodel.MainViewModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.bumptech.glide.Glide;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomepageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomepageFragment extends Fragment {
 
-    private MainViewModel mMainViewModel;
     private FirebaseFirestore db;
     private FirebaseAuth mAuth;
-    BottomNavigationView bottomNavigationView;
+    private BottomNavigationView bottomNavigationView;
+    private ImageView randomUserImageView;
+
+    String userID;
+
+    StorageReference storageReference;
+
+    FirebaseStorage storage;
+
+    StorageReference storageReferenceFolder;
 
     public HomepageFragment() {
         // Required empty public constructor
         //
     }
 
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
     public void onStop(){
         super.onStop();
     }
@@ -55,17 +161,30 @@ public class HomepageFragment extends Fragment {
         super.onPause();
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        db = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        userID = mAuth.getUid();
+        // Create a storage reference from our app
+        storageReference = storage.getReference();
+        storageReferenceFolder = storageReference.child(userID);
+        Glide.get(requireContext()).getRegistry().prepend(StorageReference.class, InputStream.class, new StorageReferenceModelLoaderFactory());
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_homepage, container, false);
         bottomNavigationView = v.findViewById(R.id.bottomNavigationView);
+        randomUserImageView = v.findViewById(R.id.randomUserImageView);
+        // Fetch a random user's image URL and load it into the ImageView
+        fetchRandomUserImage();
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment fragment = null;
                 int id = item.getItemId();
                 if (id == R.id.AddFragment) {
@@ -74,22 +193,103 @@ public class HomepageFragment extends Fragment {
                     fragment = new DiscoverFragment();
                 } else if (id == R.id.ProfileFragment) {
                     fragment = new ProfileFragment();
+                    // Hide or clear the ImageView in the ProfileFragment
+                    // You can also set the image to null or an empty image resource
+                    // randomUserImageView.setImageDrawable(null);
                 } else if (id == R.id.HomepageFragment) {
                     fragment = new HomepageFragment();
                 }
-             getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.homepage_frag_container, fragment).commit();
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.homepage_frag_container, fragment).commit();
 
                 return true;
             }
         });
         return v;
     }
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
 
+
+    private RequestListener<Drawable> glideRequestListener = new RequestListener<Drawable>() {
+        @Override
+        public boolean onLoadFailed(GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+            // Handle the load failure here
+            Log.e("GlideError", "Load failed: " + e.getMessage());
+            return false; // Return false to allow Glide to handle the error, or true to indicate that you've handled it.
+        }
+
+        @Override
+        public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+            // Image loaded successfully
+            return false;
+        }
+    };
+
+    private void fetchRandomUserImage() {
+
+        // Step 1: Query the collection of user IDs
+        CollectionReference userCollection = db.collection("images"); // Replace "your_document_id" with the actual document ID
+        userCollection.get().addOnSuccessListener(queryDocumentSnapshots -> {
+            if (!queryDocumentSnapshots.isEmpty()) {
+                List<String> userIds = new ArrayList<>();
+
+                // Step 2: Retrieve all user IDs
+                for (DocumentSnapshot document : queryDocumentSnapshots.getDocuments()) {
+                    String userId = document.getId();
+                    userIds.add(userId);
+                }
+
+                if (!userIds.isEmpty()) {
+                    // Step 3: Select a random user ID
+                    Random random = new Random();
+                    int randomIndex = random.nextInt(userIds.size());
+                    String randomUserId = userIds.get(randomIndex);
+
+                    // Save the random user ID to the 'userID' variable
+                    userID = randomUserId;
+
+                    // Now you have a random user ID saved in the 'userID' variable.
+                } else {
+                    // Handle the case where there are no user IDs in the collection.
+                    Log.e("Firestore", "No user IDs found in the 'users' collection.");
+                }
+            } else {
+                // Handle the case where no documents were found in the collection.
+                Log.e("Firestore", "No documents found in the 'users' collection.");
+            }
+        }).addOnFailureListener(e -> {
+            // Handle Firestore query failure
+            Log.e("Firestore", "Firestore query failed: " + e.getMessage());
+        });
+
+
+
+
+
+        DocumentReference documentReference  = db.collection("images").document(userID);
+        documentReference.get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        Map<String, Object> data = documentSnapshot.getData();
+                        if (data != null) {
+                            // Access fields in the data map
+                            String imageUrl = (String) data.get("imageUrl");
+                            StorageReference imageRef = storageReference.child(imageUrl);
+                            Glide.with(requireContext())
+                                    .load(imageRef) // image ref
+                                    .listener(glideRequestListener) // Attach the listener here
+                                    .into(randomUserImageView);  // imageview object
+                        }
+                    } else {
+                        Log.d("TAG","Document Does not Exist");
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    // Handle the error
+                    Log.e("Firestore", "Error getting document", e);
+                });
     }
 
-}
 
+
+
+}
 
--- app/src/main/java/com/example/stylespo/view/LoginFragment.java
+++ app/src/main/java/com/example/stylespo/view/LoginFragment.java
@@ -2,19 +2,13 @@ package com.example.stylespo.view;
 
 import android.app.Activity;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStoreOwner;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
-
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,7 +16,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.stylespo.R;
 import com.example.stylespo.model.User;
 import com.example.stylespo.viewmodel.MainViewModel;
@@ -32,67 +25,59 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-/**
- * A simple {@link Fragment} subclass.
- * create an instance of this fragment.
- */
 public class LoginFragment extends Fragment implements View.OnClickListener {
-
 
     private MainViewModel mMainViewModel;
     private Button mSignUpButton;
     private Button mLoginButton;
     private Button mForgotPasswordButton;
     private TextView mPassword;
-    private  TextView mEmail;
+    private TextView mEmail;
     private User userModel;
     private FirebaseAuth mAuth;
+
     public LoginFragment() {
         // Required empty public constructor
     }
+
+    public void setDisable(){
+        mEmail.setEnabled(false);
+        mPassword.setEnabled(false);
+        mLoginButton.setEnabled(false);
+        mForgotPasswordButton.setEnabled(false); // Disable the "Forgot Password" button
+    }
+
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        if (currentUser != null){
+        if (currentUser != null) {
             FragmentManager fm = getActivity().getSupportFragmentManager();
             FragmentTransaction transaction = fm.beginTransaction();
+            setDisable();
             Fragment fragment = new HomepageFragment();
             transaction.setReorderingAllowed(true);
             transaction.replace(R.id.login_frag_container, fragment).commit();
         }
     }
 
-    public void onStop(){
-        super.onStop();
-    }
-
-    public void onPause(){
-        super.onPause();
-    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Activity activity = requireActivity();
-        mMainViewModel = new ViewModelProvider((ViewModelStoreOwner)activity).get(MainViewModel.class);
+        mMainViewModel = new ViewModelProvider((ViewModelStoreOwner) activity).get(MainViewModel.class);
         mMainViewModel.getUserDetails().observe(this, new Observer<User>() {
-                    @Override
-                    public void onChanged(User userModel){
-                        Toast.makeText(getActivity(), "Success", Toast.LENGTH_SHORT).show();
-                    }
-
-                }
-        );
+            @Override
+            public void onChanged(User userModel) {
+                Toast.makeText(getActivity(), "Success", Toast.LENGTH_SHORT).show();
+            }
+        });
         mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        //binding= DataBindingUtil.inflate(inflater,R.layout.fragment_main,container,false);
-        //view=binding.getRoot();
         View v = inflater.inflate(R.layout.fragment_login, container, false);
         mSignUpButton = v.findViewById(R.id.makeAccount);
         mLoginButton = v.findViewById(R.id.log_in_button);
@@ -107,54 +92,48 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
 
     public void onClick(View v) {
         final int viewId = v.getId();
-        if(viewId == R.id.makeAccount) {
+        if (viewId == R.id.makeAccount) {
             Toast.makeText(getActivity(), "Navigating to signup page", Toast.LENGTH_SHORT).show();
-            Fragment newFragment = new SignUpFragment(); // Instantiate the new fragment
+            setDisable();
+            Fragment newFragment = new SignUpFragment();
             FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction();
             transaction.replace(R.id.login_frag_container, newFragment);
-            transaction.addToBackStack(null); // Optional: Adds the transaction to the back stack
+            transaction.addToBackStack(null);
             transaction.commit();
         } else if (viewId == R.id.log_in_button) {
-            if(TextUtils.isEmpty(String.valueOf(mEmail.getText()))){
-                Toast.makeText(getActivity(),"Enter Email", Toast.LENGTH_SHORT).show();
+            if (TextUtils.isEmpty(String.valueOf(mEmail.getText()))) {
+                Toast.makeText(getActivity(), "Enter Email", Toast.LENGTH_SHORT).show();
                 return;
             }
-            if (TextUtils.isEmpty(String.valueOf(mPassword.getText()))){
-                Toast.makeText(getActivity(),"Enter Password", Toast.LENGTH_SHORT).show();
+            if (TextUtils.isEmpty(String.valueOf(mPassword.getText()))) {
+                Toast.makeText(getActivity(), "Enter Password", Toast.LENGTH_SHORT).show();
                 return;
             }
-            mAuth.signInWithEmailAndPassword(mEmail.getText().toString(),mPassword.getText().toString())
+            mAuth.signInWithEmailAndPassword(mEmail.getText().toString(), mPassword.getText().toString())
                     .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                Toast.makeText(getActivity(), "Login successfull.",
-                                        Toast.LENGTH_SHORT).show();
-                                    FragmentManager fm = getActivity().getSupportFragmentManager();
-                                    FragmentTransaction transaction = fm.beginTransaction();
-                                    Fragment fragment = new HomepageFragment();
-                                    transaction.setReorderingAllowed(true);
-                                    transaction.replace(R.id.login_frag_container, fragment).commit();
+                                Toast.makeText(getActivity(), "Login successful.", Toast.LENGTH_SHORT).show();
+                                FragmentManager fm = getActivity().getSupportFragmentManager();
+                                FragmentTransaction transaction = fm.beginTransaction();
+                                setDisable();
+                                Fragment fragment = new HomepageFragment();
+                                transaction.addToBackStack(null);
+                                transaction.setReorderingAllowed(true);
+                                transaction.replace(R.id.login_frag_container, fragment).commit();
                             } else {
-                                Toast.makeText(getActivity(), "login failed.",
-                                        Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getActivity(), "Login failed.", Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
-
-
         } else if (viewId == R.id.forgot_password) {
             Toast.makeText(getActivity(), "Navigating to forgot password page", Toast.LENGTH_SHORT).show();
-            Fragment newFragment = new ForgotPasswordFragment(); // Instantiate the new fragment
+            Fragment newFragment = new ForgotPasswordFragment();
             FragmentTransaction transaction = requireActivity().getSupportFragmentManager().beginTransaction();
             transaction.replace(R.id.login_frag_container, newFragment);
-            transaction.addToBackStack(null); // Optional: Adds the transaction to the back stack
+            transaction.addToBackStack(null);
             transaction.commit();
         }
     }
 }
-
-//for onDestroy just call super
-
-//jetpack nav
-//viewbinding more than we need
